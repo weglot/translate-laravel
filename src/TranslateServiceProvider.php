30522c724897043776a5d11eaf7c77920c5a10a1
Baptiste Leduc <baptiste@weglot.com>
2018-04-27 16:21:42 +0200

    removing useless ViewServiceProvider overwrite & registering BladeCompiler from TranslateServiceProvider

--- src/TranslateServiceProvider.php
+++ src/TranslateServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Weglot\Translate;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\View\Engines\CompilerEngine;
+use Weglot\Translate\Compilers\BladeCompiler;
 
 /**
  * Class TranslateServiceProvider
@@ -17,6 +19,7 @@ class TranslateServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // publish custom configuration
         $this->publishes([
             __DIR__ . '/config.php' => config_path('weglot-translate.php'),
         ]);
@@ -29,8 +32,20 @@ class TranslateServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // using custom configuration
         $this->mergeConfigFrom(
             __DIR__ . '/config.php', 'weglot-translate'
         );
+
+        // adding our custom blade compiler class to view service
+        $app = $this->app;
+        $resolver = $app['view']->getEngineResolver();
+
+        $resolver->register('blade', function() use ($app)
+        {
+            $compiler = new BladeCompiler($app['files'], $app['config']['view.compiled']);
+
+            return new CompilerEngine($compiler);
+        });
     }
 }
