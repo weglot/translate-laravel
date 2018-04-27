344394ec60011896718a8d6afe7356596d975486
Baptiste Leduc <baptiste@weglot.com>
2018-04-27 16:09:28 +0200

    implementing weglot/weglot-php

--- src/Compilers/BladeCompiler.php
+++ src/Compilers/BladeCompiler.php
@@ -4,6 +4,9 @@ namespace Weglot\Translate\Compilers;
 
 use Illuminate\View\Compilers\BladeCompiler as LaravelBladeCompiler;
 use Illuminate\View\Compilers\CompilerInterface;
+use Weglot\Client\Client;
+use Weglot\Parser\ConfigProvider\ServerConfigProvider;
+use Weglot\Parser\Parser;
 
 /**
  * Class BladeCompiler
@@ -14,13 +17,24 @@ class BladeCompiler extends LaravelBladeCompiler implements CompilerInterface
     /**
      * Compile the given Blade template contents.
      *
-     * @param  string  $value
+     * @param string $value
      * @return string
+     * @throws \Psr\Cache\InvalidArgumentException
+     * @throws \Weglot\Client\Api\Exception\ApiError
+     * @throws \Weglot\Client\Api\Exception\InputAndOutputCountMatchException
+     * @throws \Weglot\Client\Api\Exception\InvalidWordTypeException
+     * @throws \Weglot\Client\Api\Exception\MissingRequiredParamException
+     * @throws \Weglot\Client\Api\Exception\MissingWordsOutputException
      */
     public function compileString($value)
     {
         $contents = parent::compileString($value);
+        $config = config('weglot-translate');
 
-        return $contents;
+        $client = new Client($config['api_key']);
+        $configProvider = new ServerConfigProvider();
+
+        $parser = new Parser($client, $configProvider, $config['exclude_blocks']);
+        return $parser->translate($contents, 'en', 'fr');
     }
 }
