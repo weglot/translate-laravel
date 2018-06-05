0b046f8a14dd889a129fecb369b06a760aea383b
Baptiste Leduc <baptiste@weglot.com>
2018-06-05 17:15:12 +0200

    implementing new routing for both templates

--- resources/views/language-button-2.blade.php
+++ resources/views/language-button-2.blade.php
@@ -1,3 +1,3 @@
 @foreach ($urls as $lang => $url)
-    <a href="{{ url($url) }}" data-wg-notranslate class="weglot-link weglot-{{ $lang }}">{{ weglotLanguage($lang, false) }}</a>
+    <a href="{{ route(Route::current()->getName(), ['_wg_lang' => $lang]) }}" data-wg-notranslate class="weglot-link weglot-{{ $lang }}">{{ weglotLanguage($lang, false) }}</a>
 @endforeach
